ed672152d0fdeffe27b8d00ef814b376b2fd7164
chanbokW <cksqhr4961@naver.com>
2022-09-23 22:24:09 +0900

    feat: create Member Entity

---
+++ src/main/java/me/snsservice/member/domain/Member.java
@@ -0,0 +1,36 @@
+package me.snsservice.member.domain;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import me.snsservice.common.domain.BaseTimeEntity;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private String nickname;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Builder
+    public Member(String email, String nickname, String password, Role role) {
+        this.email = email;
+        this.nickname = nickname;
+        this.password = password;
+        this.role = role;
+    }
+}
