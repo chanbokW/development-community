22a8df3ed0039a12335acdad2189e47938d6b55a
chanbokW <cksqhr4961@naver.com>
2022-09-23 17:54:56 +0900

    init project

---
+++ src/test/java/me/snsservice/SnsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.snsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
