7df217c8aec829f23b108e9ae47f511620aa4666
chanbokW <cksqhr4961@naver.com>
2022-10-07 11:38:16 +0900

    feat:[#15] 게시물 API 생성

--- src/main/java/me/snsservice/article/dto/UpdateArticleRequest.java
+++ src/main/java/me/snsservice/article/dto/UpdateArticleRequest.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public class ArticleUpdateRequest {
+public class UpdateArticleRequest {
 
     private String title;
     private String content;
